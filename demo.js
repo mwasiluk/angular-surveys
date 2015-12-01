65ae28cc261f422bbc86e44ade96713f01e6e5a2
mwasiluk <m.wasiluk89@gmail.com>
2015-12-01 01:03:48 +0100

    init

---
+++ demo.js
@@ -0,0 +1,52 @@
+angular.module('app', ['mwFormBuilder', 'mwFormViewer', 'pascalprecht.translate', 'monospaced.elastic'])
+    .config(function($translateProvider){
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'https://rawgit.com/mwasiluk/angular-surveys/master/dist/i18n/',
+            suffix: '.json'
+        });
+        $translateProvider.preferredLanguage('en');
+    })
+    .controller('DemoController', function($q, $translate) {
+        var ctrl = this;
+        ctrl.languages = ['en', 'pl'];
+        ctrl.formData = {};
+        ctrl.formViewer = {};
+        ctrl.formStatus= {};
+        ctrl.responseData={};
+        ctrl.showResponseRata=false;
+        ctrl.saveResponse = function(){
+            var d = $q.defer();
+            var res = confirm("Response save success?");
+            if(res){
+                d.resolve(true);
+            }else{
+                d.reject();
+            }
+
+            return d.promise;
+        };
+
+        ctrl.onImageSelection = function (){
+
+            var d = $q.defer();
+            var src = prompt("Please enter image src");
+            if(src !=null){
+                d.resolve(src);
+            }else{
+                d.reject();
+            }
+
+            return d.promise;
+        };
+
+        ctrl.resetViewer = function(){
+            if(ctrl.formViewer.reset){
+                ctrl.formViewer.reset();
+            }
+
+        };
+
+        ctrl.changeLanguage = function (languageKey) {
+            $translate.use(languageKey);
+        };
+    });
