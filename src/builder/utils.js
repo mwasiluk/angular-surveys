2cbcc443297e3e7bb5950016a84076de4667c4be
Oscar Serna <oscar.serna@novaip.com.co>
2016-03-09 10:30:33 -0500

    clone page and clone form on util

--- src/builder/utils.js
+++ src/builder/utils.js
@@ -1,69 +1,85 @@
 angular.module('mwFormBuilder')
         .service('mwFormUuid', function () {
-            function s4() {
-                return Math.floor((1 + Math.random()) * 0x10000)
-                        .toString(16)
-                        .substring(1);
-            }
-            this.get = function () {
-                return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+          function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+          }
+          this.get = function () {
+            return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
 
 
-            };
+          };
         }).factory('mwFormClone', ["mwFormUuid", function (mwFormUuid) {
-        var service = {};
-        var checkedObjects = [];
-        function resetIds(obj, root) {
-            if (root) {
-                checkedObjects = [];
-            }
-            if (checkedObjects.indexOf(obj) >= 0) {
-                return;
-            }
-            checkedObjects.push(obj);
-            if (!obj === Object(obj)) {
-                return;
-            }
+    var service = {};
+    var checkedObjects = [];
+    function resetIds(obj, root) {
+      if (root) {
+        checkedObjects = [];
+      }
+      if (checkedObjects.indexOf(obj) >= 0) {
+        return;
+      }
+      checkedObjects.push(obj);
+      if (!obj === Object(obj)) {
+        return;
+      }
 
-            if (Array.isArray(obj)) {
-                obj.forEach(resetIds);
-                return;
-            }
+      if (Array.isArray(obj)) {
+        obj.forEach(resetIds);
+        return;
+      }
 
-            for (var property in obj) {
-                if (obj.hasOwnProperty(property)) {
-                    resetIds(obj[property]);
-                }
-            }
-
-            if (obj.hasOwnProperty('id')) {
-                var newId = mwFormUuid.get();
-                var oldId = obj.id;
-                obj.id = newId;
-            }
-
+      for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+          resetIds(obj[property]);
         }
-        service.cloneElement = function (pageElement) {
-            var element = {};
-            angular.copy(pageElement, element);
-            resetIds(element, true);
-            return element;
-        };
+      }
 
-        service.cloneElements = function (elements) {
-            var _elements = [];
-            if (Array.isArray(elements)) {
-                for (var i = 0; i < elements.length; i++) {
-                    _elements.push(service.cloneElement(elements[i]));
-                }
+      if (obj.hasOwnProperty('id')) {
+        var newId = mwFormUuid.get();
+        var oldId = obj.id;
+        obj.id = newId;
+      }
 
-            }
-            return _elements;
-        };
-        
-        return service;
+    }
+    
+    service.cloneElement = function (pageElement) {
+      var element = {};
+      angular.copy(pageElement, element);
+      resetIds(element, true);
+      return element;
+    };
 
-    }]);
+    service.clonePage = function (formPage) {
+      var _page = {};
+      angular.copy(formPage, _page);
+      _page.id = mwFormUuid.get();
+      var _elements = [];
+      if (Array.isArray(formPage.elements)) {
+        for (var i = 0; i < formPage.elements.length; i++) {
+          _elements.push(service.cloneElement(formPage.elements[i]));
+        }
+      }
+      _page.elements = _elements;
+      return _page;
+    };
+
+    service.cloneForm = function (form) {
+      var _form = {};
+      angular.copy(form, _form);
+      var _pages = [];
+      if (Array.isArray(form.pages)) {
+        for (var i = 0; i < form.pages.length; i++) {
+          _pages.push(service.clonePage(form.pages[i]));
+        }
+      }
+      return _form;
+    };
+
+    return service;
+
+  }]);
 
 /*    .factory('CollectionsUtil', function(){
  return{
