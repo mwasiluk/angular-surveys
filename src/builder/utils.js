b6d6773d1ebc46722ff30fefd0899737b1d81e59
Jorge Villada <jorge.villada@novaip.com.co>
2016-03-09 09:40:40 -0500

    util to clone elements. deleted  from directive.

--- src/builder/utils.js
+++ src/builder/utils.js
@@ -1,36 +1,90 @@
 angular.module('mwFormBuilder')
-    .service('mwFormUuid', function () {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-        this.get = function () {
-            return s4() + s4()  + s4() + s4() +s4() + s4() + s4() + s4();
-
-
-        };
-    });
-/*    .factory('CollectionsUtil', function(){
-        return{
-            replaceByEqualObject: function(collection, referenceObjects, equalFn){
-                return _.map(collection, function(element){
-                    var foundRefObj = _.find(referenceObjects, function(refObj){
-                        if(equalFn){
-                            return equalFn(element, refObj);
-                        }else{
-                            return _.isEqual(element, refObj);
-                        }
-                    });
-                    if(foundRefObj){
-                        return foundRefObj;
-                    }else{
-                        return element;
-                    }
-                });
-            },
-            isNotEmptyArray: function(object){
-                return object && $.isArray(object) && object.length > 0;
+        .service('mwFormUuid', function () {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1);
             }
+            this.get = function () {
+                return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+
+
+            };
+        }).factory('mwFormClone', ["mwFormUuid", function (mwFormUuid) {
+        var service = {};
+        var checkedObjects = [];
+        function resetIds(obj, root) {
+            if (root) {
+                checkedObjects = [];
+            }
+            if (checkedObjects.indexOf(obj) >= 0) {
+                return;
+            }
+            checkedObjects.push(obj);
+            if (!obj === Object(obj)) {
+                return;
+            }
+
+            if (Array.isArray(obj)) {
+                obj.forEach(resetIds);
+                return;
+            }
+
+            for (var property in obj) {
+                if (obj.hasOwnProperty(property)) {
+                    resetIds(obj[property]);
+                }
+            }
+
+            if (obj.hasOwnProperty('id')) {
+                var newId = mwFormUuid.get();
+                var oldId = obj.id;
+                obj.id = newId;
+            }
+
         }
-    });*/
+        service.cloneElement = function (pageElement) {
+            var element = {};
+            angular.copy(pageElement, element);
+            resetIds(element, true);
+            return element;
+        };
+
+        service.cloneElements = function (elements) {
+            var _elements = [];
+            if (Array.isArray(elements)) {
+                for (var i = 0; i < elements.length; i++) {
+                    _elements.push(service.cloneElement(elements[i]));
+                }
+
+            }
+            return _elements;
+        };
+        
+        return service;
+
+    }]);
+
+/*    .factory('CollectionsUtil', function(){
+ return{
+ replaceByEqualObject: function(collection, referenceObjects, equalFn){
+ return _.map(collection, function(element){
+ var foundRefObj = _.find(referenceObjects, function(refObj){
+ if(equalFn){
+ return equalFn(element, refObj);
+ }else{
+ return _.isEqual(element, refObj);
+ }
+ });
+ if(foundRefObj){
+ return foundRefObj;
+ }else{
+ return element;
+ }
+ });
+ },
+ isNotEmptyArray: function(object){
+ return object && $.isArray(object) && object.length > 0;
+ }
+ }
+ });*/
