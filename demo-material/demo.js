e71a073b403bc5f0d37d8be390166f94f04ef489
mwasiluk <m.wasiluk89@gmail.com>
2016-09-05 12:49:46 +0200

    activated russian translation in material demo

--- demo-material/demo.js
+++ demo-material/demo.js
@@ -17,7 +17,7 @@ angular.module('app', ['ngMaterial', 'mwFormBuilder', 'mwFormViewer', 'mwFormUti
         ctrl.headersWithQuestionNumber = true;
         ctrl.builderReadOnly = false;
         ctrl.viewerReadOnly = false;
-        ctrl.languages = ['en', 'pl', "es"];
+        ctrl.languages = ['en', 'pl', "es", 'ru'];
         ctrl.formData = null;
         $http.get('form-data.json')
             .then(function(res){
