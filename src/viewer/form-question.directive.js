261730571cfd08b283dd2b3f1d62c35ae8275d7a
mindnervestech <mindnervestech@gmail.com>
2018-02-02 18:16:39 +0530

    feature file upload functionality

--- src/viewer/form-question.directive.js
+++ src/viewer/form-question.directive.js
@@ -1,143 +1,168 @@
-
-angular.module('mwFormViewer').factory("FormQuestionId", function(){
-    var id = 0;
+angular.module('mwFormViewer').factory("FormQuestionId", function() {
+        var id = 0;
         return {
-            next: function(){
+            next: function() {
                 return ++id;
             }
         }
     })
 
-    .directive('mwFormQuestion', function () {
+    .directive('mwFormQuestion', ['$parse', function($parse) {
 
-    return {
-        replace: true,
-        restrict: 'AE',
-        require: '^mwFormViewer',
-        scope: {
-            question: '=',
-            questionResponse: '=',
-            readOnly: '=?',
-            options: '=?',
+        return {
+            replace: true,
+            restrict: 'AE',
+            require: '^mwFormViewer',
+            scope: {
+                question: '=',
+                questionResponse: '=',
+                readOnly: '=?',
+                options: '=?',
             onResponseChanged: '&?'
-        },
-        templateUrl: 'mw-form-question.html',
-        controllerAs: 'ctrl',
-        bindToController: true,
-        controller: function($timeout,FormQuestionId){
-            var ctrl = this;
+            },
+            templateUrl: 'mw-form-question.html',
+            controllerAs: 'ctrl',
+            bindToController: true,
+            controller: ["$timeout", "FormQuestionId", function($timeout, FormQuestionId) {
+                var ctrl = this;
+                ctrl.largeFileFlag = false;
+                // Put initialization logic inside `$onInit()`
+                // to make sure bindings have been initialized.
+                this.$onInit = function() {
+                    ctrl.id = FormQuestionId.next();
 
-            // Put initialization logic inside `$onInit()`
-            // to make sure bindings have been initialized.
-            this.$onInit = function() {
-                ctrl.id = FormQuestionId.next();
+                    if (ctrl.question.type == 'radio') {
+                        /*if (!ctrl.questionResponse.selectedAnswer) {
+                            ctrl.questionResponse.selectedAnswer = null;
+                        }*/
+                        if (ctrl.questionResponse.other) {
+                            ctrl.isOtherAnswer = true;
+                        }
 
-                if(ctrl.question.type=='radio'){
-                    if(!ctrl.questionResponse.selectedAnswer){
-                        ctrl.questionResponse.selectedAnswer=null;
+                    } else if (ctrl.question.type == 'checkbox') {
+                        if (ctrl.questionResponse.selectedAnswers && ctrl.questionResponse.selectedAnswers.length) {
+                            ctrl.selectedAnswer = true;
+                        } else {
+                            ctrl.questionResponse.selectedAnswers = [];
+                        }
+                        if (ctrl.questionResponse.other) {
+                            ctrl.isOtherAnswer = true;
+                        }
+
+
+                    } else if (ctrl.question.type == 'grid') {
+                        if (!ctrl.question.grid.cellInputType) {
+                            ctrl.question.grid.cellInputType = "radio";
+                        }
+                        //if(ctrl.questionResponse.selectedAnswers){
+                        //
+                        //}else{
+                        //    ctrl.questionResponse.selectedAnswers={};
+                        //}
+                    } else if (ctrl.question.type == 'division') {
+
+                        ctrl.computeDivisionSum = function() {
+                            ctrl.divisionSum = 0;
+                            ctrl.question.divisionList.forEach(function(item) {
+
+                                if (ctrl.questionResponse[item.id] != 0 && !ctrl.questionResponse[item.id]) {
+                                    ctrl.questionResponse[item.id] = null;
+                                    ctrl.divisionSum += 0;
+                                } else {
+                                    ctrl.divisionSum += ctrl.questionResponse[item.id];
+                                }
+                            });
+                        };
+
+                        ctrl.computeDivisionSum();
+
+
+                    } else if (ctrl.question.type == 'date' || ctrl.question.type == 'datetime' || ctrl.question.type == 'time') {
+                        if (ctrl.questionResponse.answer) {
+                            ctrl.questionResponse.answer = new Date(ctrl.questionResponse.answer)
+                        }
+                    } else if (ctrl.question.type == 'file') {
+                        ctrl.questionResponse.fileName_1 = ctrl.questionResponse.fileName_1;
+                        ctrl.questionResponse.fileName = ctrl.questionResponse.fileName_1;
                     }
-                    if(ctrl.questionResponse.other){
-                        ctrl.isOtherAnswer=true;
-                    }
 
-                }else if(ctrl.question.type=='checkbox'){
-                    if(ctrl.questionResponse.selectedAnswers && ctrl.questionResponse.selectedAnswers.length){
-                        ctrl.selectedAnswer=true;
-                    }else{
-                        ctrl.questionResponse.selectedAnswers=[];
-                    }
-                    if(ctrl.questionResponse.other){
-                        ctrl.isOtherAnswer=true;
-                    }
+                    ctrl.isAnswerSelected = false;
+                    ctrl.initialized = true;
+                };
 
-
-                }else if(ctrl.question.type=='grid'){
-                    if(!ctrl.question.grid.cellInputType){
-                        ctrl.question.grid.cellInputType = "radio";
-                    }
-                    //if(ctrl.questionResponse.selectedAnswers){
-                    //
-                    //}else{
-                    //    ctrl.questionResponse.selectedAnswers={};
-                    //}
-                }else if(ctrl.question.type=='division'){
-
-                    ctrl.computeDivisionSum = function(){
-                        ctrl.divisionSum=0;
-                        ctrl.question.divisionList.forEach(function(item){
-
-                            if(ctrl.questionResponse[item.id]!=0 && !ctrl.questionResponse[item.id]){
-                                ctrl.questionResponse[item.id]=null;
-                                ctrl.divisionSum+=0;
-                            }else{
-                                ctrl.divisionSum+=ctrl.questionResponse[item.id];
-                            }
-                        });
-                    };
-
-                    ctrl.computeDivisionSum();
-
-
-                }else if(ctrl.question.type=='date' || ctrl.question.type=='datetime' || ctrl.question.type=='time'){
-                    if(ctrl.questionResponse.answer){
-                        ctrl.questionResponse.answer = new Date(ctrl.questionResponse.answer)
-                    }
-                }
-
-                ctrl.isAnswerSelected=false;
-                ctrl.initialized = true;
-            };
-
-            ctrl.selectedAnswerChanged=function(){
-                delete ctrl.questionResponse.other;
-                ctrl.isOtherAnswer=false;
-                ctrl.answerChanged();
-
-            };
-            ctrl.otherAnswerRadioChanged= function(){
-                console.log('otherAnswerRadioChanged');
-                if(ctrl.isOtherAnswer){
-                    ctrl.questionResponse.selectedAnswer=null;
-                }
-                ctrl.answerChanged();
-            };
-
-            ctrl.otherAnswerCheckboxChanged= function(){
-                if(!ctrl.isOtherAnswer){
+                ctrl.selectedAnswerChanged = function() {
                     delete ctrl.questionResponse.other;
+                    ctrl.isOtherAnswer = false;
+                    ctrl.answerChanged();
+
+                };
+                ctrl.otherAnswerRadioChanged = function() {
+                    if (ctrl.isOtherAnswer) {
+                        ctrl.questionResponse.selectedAnswer = null;
+                    }
+                    ctrl.answerChanged();
+                };
+
+                ctrl.otherAnswerCheckboxChanged = function() {
+                    if (!ctrl.isOtherAnswer) {
+                        delete ctrl.questionResponse.other;
+                    }
+                    ctrl.selectedAnswer = ctrl.questionResponse.selectedAnswers.length || ctrl.isOtherAnswer ? true : null;
+                    ctrl.answerChanged();
+                };
+
+
+                ctrl.toggleSelectedAnswer = function(answer) {
+                    if (ctrl.questionResponse.selectedAnswers.indexOf(answer.id) === -1) {
+                        ctrl.questionResponse.selectedAnswers.push(answer.id);
+                    } else {
+                        ctrl.questionResponse.selectedAnswers.splice(ctrl.questionResponse.selectedAnswers.indexOf(answer.id), 1);
+                    }
+                    ctrl.selectedAnswer = ctrl.questionResponse.selectedAnswers.length || ctrl.isOtherAnswer ? true : null;
+
+                    ctrl.answerChanged();
+                };
+
+                ctrl.answerChanged = function() {
+                    if (ctrl.onResponseChanged) {
+                        ctrl.onResponseChanged();
+                    }
                 }
-                ctrl.selectedAnswer = ctrl.questionResponse.selectedAnswers.length||ctrl.isOtherAnswer ? true:null ;
-                ctrl.answerChanged();
-            };
 
-
-            ctrl.toggleSelectedAnswer= function(answer){
-                if (ctrl.questionResponse.selectedAnswers.indexOf(answer.id) === -1) {
-                    ctrl.questionResponse.selectedAnswers.push(answer.id);
-                } else {
-                    ctrl.questionResponse.selectedAnswers.splice(ctrl.questionResponse.selectedAnswers.indexOf(answer.id), 1);
+                // Prior to v1.5, we need to call `$onInit()` manually.
+                // (Bindings will always be pre-assigned in these versions.)
+                if (angular.version.major === 1 && angular.version.minor < 5) {
+                    this.$onInit();
                 }
-                ctrl.selectedAnswer = ctrl.questionResponse.selectedAnswers.length||ctrl.isOtherAnswer ? true:null ;
 
-                ctrl.answerChanged();
-            };
+            }],
+            link: function(scope, ele, attrs, mwFormViewer) {
+                var ctrl = scope.ctrl;
+                ctrl.print = mwFormViewer.print;
 
-            ctrl.answerChanged = function(){
-                if(ctrl.onResponseChanged){
-                    ctrl.onResponseChanged();
-                }
+                //file uploads
+                //
+
+                ele.bind("change", function(changeEvent) {
+                    var fileSize = changeEvent.target.files[0].size / 1024;
+                    console.log("file size.....................",fileSize);
+                    if (fileSize <= 1024) {
+                        ctrl.largeFileFlag = false;
+                        var reader = new FileReader();
+                        var fileName = changeEvent.target.files[0];
+                        reader.onload = function(loadEvent) {
+                            scope.$apply(function() {
+                                ctrl.questionResponse.answer = loadEvent.target.result;
+                                ctrl.questionResponse.fileName = changeEvent.target.files[0].name;
+                            });
+                        }
+                        
+                        reader.readAsDataURL(changeEvent.target.files[0]); 
+                    } else {
+                        ctrl.largeFileFlag = true;   
+                        alert("File size is larze; maximum file size 1 MB");          
+                    }
+                });
             }
-
-            // Prior to v1.5, we need to call `$onInit()` manually.
-            // (Bindings will always be pre-assigned in these versions.)
-            if (angular.version.major === 1 && angular.version.minor < 5) {
-                this.$onInit();
-            }
-
-        },
-        link: function (scope, ele, attrs, mwFormViewer){
-            var ctrl = scope.ctrl;
-            ctrl.print =  mwFormViewer.print;
-        }
-    };
-});
+        };
+    }]);
